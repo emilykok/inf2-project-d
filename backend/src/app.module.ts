762d099233845e372ca7d7b16574865a701da3d6
bjornkok <1007695@hr.nl>
2022-04-19 13:49:56 +0200

    Connected database with backend
    also additional code to facilitate requests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService, PostService],
 })
 export class AppModule {}
