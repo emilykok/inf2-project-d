12805111685b8f217a38096b7ad309d2fde04030
bjornkok <1007695@hr.nl>
2022-05-12 15:31:41 +0200

    fix
    Co-Authored-By: MelissaKros <70698504+MelissaKros@users.noreply.github.com>

--- backend/src/Contact/dto/create-contact.dto.ts
+++ backend/src/Contact/dto/create-contact.dto.ts
@@ -13,38 +13,7 @@ export class CreateContactDto {
   @IsNotEmpty()
   phone: string;
 
-  @IsUser()
-  @IsNotEmpty()
-  User: User;
-
   @IsString()
   @IsNotEmpty()
   userId: string;
 }
-
-/*
-import { IsEmail, IsNotEmpty, IsString, IsInt } from 'class-validator';
-import { IsStrongPassword } from '../../Libraries/validator/validator';
-
-
-export class CreateContactDto {
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @IsStrongPassword()
-  password: string;
-
-  @IsString()
-  @IsNotEmpty()
-  phone: string;
-
-  @IsInt()
-  clearance: number;
-}*/
