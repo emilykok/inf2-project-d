4c0c070ac3c066d6eca5d6e4aa854314e06bc3e3
bjornkok <1007695@hr.nl>
2022-06-15 11:51:45 +0200

    PUUUUUUUSH

--- backend/src/User/user.controller.spec.ts
+++ backend/src/User/user.controller.spec.ts
@@ -5,8 +5,7 @@ import { User } from '@prisma/client';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import type { PaginationQueryInput } from '@Libraries/interfaces/pagination.interface';
-//import {} from '@Libraries/unit-tests';                 //TODO: Ask stijn what to do with this error
-//import { PublicUser } from '@lib/unit-tests/types';
+import {} from '@Libraries/unit-tests/user'; //TODO: Ask stijn what to do with this error
 import { v4 as uuidv4 } from 'uuid';
 import { ForbiddenException } from '@nestjs/common';
 
