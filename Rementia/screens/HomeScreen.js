afaf34a32df912affad9d4f79d67d926ae641875
MelissaKros <1019740@hr.nl>
2022-06-15 11:56:25 +0200

    stillscreen home, and inbox added

--- Rementia/screens/HomeScreen.js
+++ Rementia/screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Picker} from '@react-native-picker/picker';
 import {
   View,
   Text,
@@ -10,8 +11,11 @@ import {
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
+var curr = new Date(); // get current date
+var date = new Date(curr.setDate(curr.getDate())).toUTCString().slice(0, 16);
+
 const HomeScreen = ({navigation}) => {
-  const [selectedValue, setSelectedValue] = React.useState('Emily');
+  const [name, setName] = React.useState('Emily');
   return (
     <SafeAreaView style={styles.background}>
       <ScrollView contentContainerStyle={{flexGrow: 1}}>
@@ -19,11 +23,45 @@ const HomeScreen = ({navigation}) => {
           <Text style={styles.titletext}> Home </Text>
         </View>
         <View style={styles.container}>
-          <View style={styles.innercontainer}>
+          <View style={[styles.innercontainer]}>
             <Text style={styles.headertext}>Goedendag Melissa</Text>
             <Text style={styles.text}>
               Je bekijkt momenteel de gegevens van:
             </Text>
+            <View style={styles.pickercontainer}>
+              <Picker
+                selectedValue={name}
+                onValueChange={(value, index) => setName(value)}
+                mode="dropdown" // Android only
+                style={styles.picker}>
+                <Picker.Item label="Selecteer een patient" value="Unknown" />
+                <Picker.Item label="Emily" value="Emily" />
+                <Picker.Item label="Viktor" value="Viktor" />
+                <Picker.Item label="Rick" value="Rick" />
+                <Picker.Item label="Henk" value="Henk" />
+                <Picker.Item label="Harrie" value="Harrie" />
+              </Picker>
+            </View>
+          </View>
+          <View style={[styles.innercontainer]}>
+            <Text style={styles.headertext}>Aankomende afspraken</Text>
+            <View style={styles.pickercontainer}>
+              <Text style={styles.smalltext}> {date} | 13:15 - 14:15</Text>
+              <Text style={styles.text}>Koffie drinken met Hu Tao</Text>
+            </View>
+            <View style={styles.pickercontainer}>
+              <Text style={styles.smalltext}> {date} | 15:35 - 16:00 </Text>
+              <Text style={styles.text}>Insuline pomp hervullen</Text>
+            </View>
+            <Text style={styles.smalltextright}>Meer afspraken...</Text>
+          </View>
+          <View style={[styles.innercontainer]}>
+            <Text style={styles.headertext}>Alerts</Text>
+
+            <View style={styles.alertcontainer}>
+              <Text style={styles.headertext}>Gezond!</Text>
+            </View>
+            <Text style={styles.smalltextright}>Meer meldingen...</Text>
           </View>
         </View>
       </ScrollView>
@@ -37,11 +75,19 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: '#008a8a',
+    marginBottom: 50,
   },
   innercontainer: {
     backgroundColor: '#A2DCDD',
     padding: 20,
     borderRadius: 10,
+    marginBottom: 5,
+  },
+  pickercontainer: {
+    backgroundColor: '#E4E4E4',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 5,
   },
   headertext: {
     fontSize: 20,
@@ -65,4 +111,20 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
   },
+  smalltext: {
+    fontSize: 10,
+    color: '#008A8A',
+  },
+  smalltextright: {
+    fontSize: 10,
+    color: '#008A8A',
+    textAlign: 'right',
+  },
+  alertcontainer: {
+    backgroundColor: '#7edc6b',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  picker: {},
 });
--- Rementia/screens/InboxScreen.js
+++ Rementia/screens/InboxScreen.js
@@ -11,6 +11,11 @@ import {
   ScrollView,
 } from 'react-native';
 
+var curr = new Date(); // get current date
+var yesterday = new Date(curr.setDate(curr.getDate() - 1))
+  .toUTCString()
+  .slice(0, 16);
+
 const InboxScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.background}>
@@ -19,11 +24,46 @@ const InboxScreen = ({navigation}) => {
           <Text style={styles.titletext}> Inbox </Text>
         </View>
         <View style={styles.container}>
-          <View style={styles.innercontainer}>
-            <Text style={styles.headertext}>Welcome!</Text>
+          <View style={styles.inboxMessage}>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 1,
+                justifyContent: 'space-between',
+              }}>
+              <Text style={styles.headertext}>Alert: hoog</Text>
+              <Text style={styles.smalltext}>Markeren als gelezen</Text>
+            </View>
             <Text style={styles.text}>
-              This is the inbox screen of our first application, this will have
-              many new features coming soon ;)
+              Emily haar glucosewaardes waren op {yesterday} om 9:00 te hoog.
+            </Text>
+          </View>
+          <View style={styles.inboxMessageRead}>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 1,
+                justifyContent: 'space-between',
+              }}>
+              <Text style={styles.headertext}>Alert: laag</Text>
+              <Text style={styles.smalltext}>Markeren als ongelezen</Text>
+            </View>
+            <Text style={styles.text}>
+              Emily haar glucosewaardes waren op {yesterday} om 12:00 te laag.
+            </Text>
+          </View>
+          <View style={styles.inboxMessageRead}>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 1,
+                justifyContent: 'space-between',
+              }}>
+              <Text style={styles.headertext}>Alert: laag</Text>
+              <Text style={styles.smalltext}>Markeren als ongelezen</Text>
+            </View>
+            <Text style={styles.text}>
+              Emily haar glucosewaardes waren op {yesterday} om 15:00 te laag.
             </Text>
           </View>
         </View>
@@ -39,10 +79,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#008a8a',
   },
-  innercontainer: {
+  inboxMessage: {
     backgroundColor: '#A2DCDD',
     padding: 20,
     borderRadius: 10,
+    marginBottom: 10,
+  },
+  inboxMessageRead: {
+    backgroundColor: '#E4E4E4',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
   },
   headertext: {
     fontSize: 20,
@@ -60,6 +107,12 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: 'left',
   },
+  smalltext: {
+    fontSize: 10,
+    textAlign: 'right',
+    color: '#008A8A',
+  },
+
   container: {
     backgroundColor: '#efefef',
     flex: 1,
